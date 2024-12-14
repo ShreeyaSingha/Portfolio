42c6ddf71aaeab8edfab1b9d2592165eec4d2240
ShreeyaSingha <sishreeya24@tbc.edu.np>
2024-12-14 20:59:30 +0545

    Add files via upload

---
+++ week1.py
@@ -0,0 +1,24 @@
+#1
+print("Hello World!")
+
+#2
+print("Hello, Shreeya!")
+
+#3
+temperature = 38.4
+print("Temperature in celsius: ", temperature)
+print("Temperature in fahrenheit: ", (temperature*9/5)+32)
+
+#4
+runs = 48426
+not_out = 162
+times_batted = 1014
+print("Boycott's Batting average = ", runs/(times_batted - not_out))
+
+#5
+numStudents1 = 113
+numStudents2 = 175
+numStudents3 = 12
+print(f"For {numStudents1} students, there will be {numStudents1//24} groups and {numStudents1%24} students left over.")
+print(f"For {numStudents1} students, there will be {numStudents2//24} groups and {numStudents2%24} students left over.")
+print(f"For {numStudents1} students, there will be {numStudents3//24} groups and {numStudents3%24} students left over.")
